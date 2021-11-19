25e5054d2522c42700d2e2bb15f012d554c9c2dc
RHenrik <henrik0314@gmail.com>
2021-11-19 11:23:43 +0100

    Főmenü ablak létrehoz.

--- Kód/Python/video.py
+++ Kód/Python/video.py
@@ -1,1 +1,17 @@
+import os
+os.environ["KIVY_NO_CONSOLELOG"] = "1"
+cwd = (os.getcwd())
+os.environ["Kivy_HOME"] = cwd + "/conf"
+
+from kivy import Config
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class MainWindow(App):
+    def build(self):
+        return Widget()
+
+
+MainWindow().run()
 
