9e63025219f32fe14b93dbfe9d6111045902c05f
RHenrik <henrik0314@gmail.com>
2021-11-19 12:21:58 +0100

    DemoMenu kivy fájl létrehoz.

--- Kód/Python/Demo.py
+++ Kód/Python/Demo.py
@@ -1,7 +1,8 @@
+from kivy.lang import Builder
 from kivy.uix.floatlayout import FloatLayout
 from kivy.app import App
 
-
+Builder.load_file("KV_Files/DemoMenu.kv")
 class MainWindow(FloatLayout):
     pass
 
