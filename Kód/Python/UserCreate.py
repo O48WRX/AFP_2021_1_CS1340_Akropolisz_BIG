2106de1f0c742d86f891cfc4847a92770e21958b
RHenrik <henrik0314@gmail.com>
2021-11-19 13:09:30 +0100

    Kollázs létrehozás menü létrehoz.

---
+++ Kód/Python/UserCreate.py
@@ -0,0 +1,19 @@
+from kivy.lang import Builder
+from kivy.uix.floatlayout import FloatLayout
+from kivy.app import App
+
+#Builder.load_file("KV_Files/DemoMenu.kv")
+
+
+class MainWindow(FloatLayout):
+    pass
+
+
+class UserCreate(App):
+    def build(self):
+        main_window = MainWindow()
+        return main_window
+
+
+usercreate = UserCreate()
+usercreate.run()
