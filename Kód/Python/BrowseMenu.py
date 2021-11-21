1ae7703ab10611f8207c4b84808116d57cc0fd11
O48WRX <kardyy81@gmail.com>
2021-11-21 10:39:38 +0100

    Kód - komment dokumentálás
    A kód kommentekkel való dokumentálása

--- Kód/Python/BrowseMenu.py
+++ Kód/Python/BrowseMenu.py
@@ -6,24 +6,30 @@ from kivy.core.window import Window
 
 Builder.load_file("KV_Files/MainMenu.kv")
 
+#itt lehet állítani színt a háttérhez.
 Window.clearcolor = (0, .6, 0, 1)
 
+#Screenek létrehozása
+#MainMenu Screen létrehozása
 class MainMenu(Screen):
     pass
-class MainWindow(FloatLayout):
-    pass
 
+#CreateCollage Screen létrehozása
 class CreateCollage(Screen):
     pass
 
+#Screen manager deklarálása
 screen_manager = ScreenManager()
 
+#Screenek hozzáadása a button-ök által hivatkozott nevekkel
 screen_manager.add_widget(MainMenu(name="main_menu"))
 screen_manager.add_widget(CreateCollage(name="user_collage"))
 
+#App létrehozása
 class VideoCreator(App):
     def build(self):
         return screen_manager
 
+#App futtatása
 vc_app = VideoCreator()
 vc_app.run()
