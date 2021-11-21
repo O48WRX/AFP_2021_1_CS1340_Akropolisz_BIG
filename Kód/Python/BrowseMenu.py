813ee874d45cdfa7bb4e2f325969e9cb2dc555c0
O48WRX <kardyy81@gmail.com>
2021-11-21 10:46:04 +0100

    Kód - nem használt import eltávolítása
    A floatlayout importot eltávolítottam, mivel nem szükséges.

--- Kód/Python/BrowseMenu.py
+++ Kód/Python/BrowseMenu.py
@@ -1,5 +1,4 @@
 from kivy.lang import Builder
-from kivy.uix.floatlayout import FloatLayout
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
