f2c3d5f00c2e6c0d872897a59051eddfc286584a
O48WRX <kardyy81@gmail.com>
2021-11-21 10:31:27 +0100

    Kód - Screenek létrehozása + manager
    A screenek és azoknak a managerének létrehozása

--- Kód/Python/BrowseMenu.py
+++ Kód/Python/BrowseMenu.py
@@ -6,15 +6,16 @@ from kivy.core.window import Window
 
 Builder.load_file("KV_Files/MainMenu.kv")
 
+Window.clearcolor = (0, .6, 0, 1)
 
+class MainMenu(Screen):
+    pass
 class MainWindow(FloatLayout):
     pass
 
+class CreateCollage(Screen):
+    pass
 
-class BrowseMenu(App):
-    def build(self):
-        main_window = MainWindow()
-        return main_window
+screen_manager = ScreenManager()
 
-browsemenu = BrowseMenu()
-browsemenu.run()
+
