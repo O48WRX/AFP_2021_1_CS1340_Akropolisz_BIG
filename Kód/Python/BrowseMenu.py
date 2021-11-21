5e6d471834e7e24510ac17dd796659c2c7aedfd0
O48WRX <kardyy81@gmail.com>
2021-11-21 10:04:30 +0100

    Kód- szükséges package-ek importálása
    A Window, ScreenManager, Screen importálása.

--- Kód/Python/BrowseMenu.py
+++ Kód/Python/BrowseMenu.py
@@ -1,6 +1,8 @@
 from kivy.lang import Builder
 from kivy.uix.floatlayout import FloatLayout
 from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.window import Window
 
 Builder.load_file("KV_Files/Browse.kv")
 
